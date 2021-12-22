45f98311d7d363dc93f80b99b319745f430e3d26
pixls <tha.pixls@gmail.com>
2021-12-22 16:14:17 -0500

    initial commit, and classes exercise done

---
+++ class-exercise/app.js
@@ -0,0 +1,48 @@
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        return "Beep."
+    }
+    toString() {
+        return `The vehicle is a ${make} ${model} from ${year}.`
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return "VROOM!!"
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+    add(vehicle) {
+        if (this.vehicles.length < this.capacity) {
+            if (vehicle instanceof Vehicle) {
+                this.vehicles.push(vehicle);
+            } else {
+                return "only put Vehicles in your garage!"
+            }
+        } else {
+            return "the garage is full!"
+        }
+    }
+}
